f78e449366f7d1dbf3d104cc0fe3724e3ed78ea4
leeyunji964 <tmxjel6279@naver.com>
2019-12-11 17:57:00 +0900

    1211

---
+++ WorkR/20191211.R
@@ -0,0 +1,204 @@
+# 12일차
+# Data Mining, Text Mining
+#
+# 워드 클라우드( Word Cloud )
+#
+# 한글 워드클라우드 설치
+# 1. java 실행환경 구축
+# 2. 자료 수집( text 자료 )
+#   2.1 text file 형태로 수집
+#   2.2 web scrapping을 이용하여 수집
+# 3. 명사 추출
+Sys.setenv( JAVA_HOME = "C:/Program Files/Java/jre1.8.0_231")
+
+#필요시 설치
+install.packages("wordcloud")       # word cloud
+install.packages("wordcloud2")      # word cloud
+install.packages("KoNLP")           # 한국어 처리 <- 한국어 처리시 반드시 필요
+install.packages("RColorBrewer")    # 색상 선택
+library( wordcloud )
+library( wordcloud2 )
+library( KoNLP )
+library( RColorBrewer )
+
+library( dplyr )
+library( ggplot2 )
+
+setwd('D:/Yunji/WorkR')
+text <- readLines( 'mis_document.txt', encoding = 'UTF-8')
+# readLines : txt 파일을 한 줄 씩 읽어와라
+text # 비정형 데이터
+
+buildDictionary( ext_dic = 'woorimalsam' ) # '우리말씀' 한글사전 로딩
+pal2 <- brewer.pal( 8, 'Dark2' ) # 색상 팔레트 생성
+noun <- sapply( text, extractNoun, USE.NAMES = F ) # 명사 추출
+noun
+# class(noun) : list
+
+# 4. 추출된 단어( 주로 명사 )에 대한 빈도수 계산 및 시각화
+noun2 <- unlist( noun ) # list -> vector로 변환
+wordcount <- table( noun2 ) 
+#wordcount
+sort.noun <- sort( wordcount, decreasing = T )[1:10]
+# 빈도가 높은 순으로 정렬, 데이터가 많으니까 데이터 셋에서 10개만 추출
+sort.noun
+sort.noun <- sort.noun[-1] # 의미없는 공백 제거
+barplot( sort.noun, names.arg = names( sort.noun ),
+         col = 'steelblue', main = '빈도수 높은 단어',
+         ylab = '단어 빈도수')
+
+df <- as.data.frame( sort.noun )
+df
+
+ggplot( df, aes( x = df$noun2, y = df$Freq ) )+
+    geom_bar( stat = 'identity',
+              width = 0.7,
+              fill = 'steelblue') +
+    ggtitle('빈도수 높은 단어') +
+    theme( plot.title = element_text( size = 25,
+                                      face = 'bold',
+                                      colour = 'steelblue',
+                                      hjust = 0,
+                                      vjust = 1 ) ) +
+    labs( x = '명사', y = '단어 빈도수' )+
+    geom_text( aes( label = df$Freq ), hjust = -0.3 ) +  #빈도 표시
+    coord_flip()
+
+# 5. word cloud 작성
+pal3 <- brewer.pal( 9, 'Blues')[5:9] # 한 가지 색으로만 색 지정
+wordcloud( names( wordcount ),   # 단어
+           freq = wordcount,     # 단어 빈도
+           scale = c(6, 0.7),    # 단어 폰트 크기(최대, 최소)
+           min.freq = 3,         # 단어 최소 빈도 : 빈도가 3 이상인 것들만 출력하라.
+           random.order = F,     # 단어 출력 위치 : T로 나오면 무작위, F면 빈도가 높은 게 가운데 지정.
+           rot.per = .1,         # 90도 회전 단어비율 : .1 = 0.1 = 10%
+           colors = pal3)        # 단어 색
+
+
+# 6. 전처리 과정 수행
+#   6.2 생략된 단어를 사전에 등재
+buildDictionary( ext_dic = 'woorimalsam',
+                 user_dic = data.frame('정치','ncn'), # ncn = 명사
+                 replace_usr_dic = T )
+noun <- sapply( text, extractNoun, USE.NAMES = F )
+noun2 <- unlist( noun )
+
+#   6.1 불필요한 단어 삭제
+noun2 <- noun2[ nchar(noun2) > 1 ]
+noun2 <- gsub('하지', '', noun2) # 하지 -> 공백으로 바꿔라
+noun2 <- gsub('때문', '', noun2) # 때문 -> 공백으로 바꿔라
+noun2 <- gsub('들이', '', noun2)
+wordcount <- table( noun2 )
+wordcount
+
+# 다시 wordcloud 실행
+pal3 <- brewer.pal( 9, 'Blues')[5:9] # 한 가지 색으로만 색 
+wordcloud( names( wordcount ),
+           freq = wordcount,
+           scale = c(6, 0.7),
+           min.freq = 3,
+           random.order = F,
+           rot.per = .1,
+           colors = pal2)
+
+
+# 애국가 형태소 분석
+library( KoNLP )
+useSystemDic()
+useSejongDic()
+useNIADic()
+
+#
+# 애국가 가사 :
+# https://mois.go.kr/frt/sub/a06/b08/nationalIcon_3/screen.do
+#
+# 1. 사전 설정 : 이게 있어야 단어 추출 가능
+useSejongDic()
+
+# 2. 텍스트 데이터 가져오기
+word_data <- readLines('애국가(가사).txt')
+word_data
+
+# 3. 명사 추출
+word_data2 <- sapply( word_data, extractNoun, USE.NAMES = F )
+word_data2 # 확인 결과, 공백(띄어쓰기)을 기준으로 해서 단어 구분 -> 정확하게 명사로 추출 X
+
+# 3.1 제대로 추출되지 않은 단어를 사용자 사전에 등록
+add_words <- c('백두산','남산','철갑','가을','하늘','달')
+buildDictionary( user_dic = data.frame( add_words,
+                            rep( 'ncn', length( add_words ) ) ),
+                 replace_usr_dic = T )
+#했는데도 안되면, 데이터 리스트에 가서 직접 바꾸는 수 밖에 없음.. 자동 추출이라서..
+
+
+# 3.2 단어 추가(3.1) 후 다시 명사 추출
+word_data2 <- sapply( word_data, extractNoun, USE.NAMES = F )
+word_data2
+
+# 4. 행렬을 벡터로 변환
+undata <- unlist( word_data2 )
+undata
+
+# 5. 사용 빈도 확인 : table()
+word_table <- table( undata )
+word_table
+
+# 6. filtering : 두 글자 이상 단어만 선별, 공백이나 한 자리 문자를 걸러냄
+undata2 <- undata[ nchar( undata ) >=2 ]
+undata2
+word_table2 <- table( undata2 )
+word_table2
+
+# 7. 데이터 정렬
+sort( word_table2, decreasing = T )
+
+# 애국가 형태 분석 완료
+# 가장 기본적인 전처리만 수행, 100% 정확한 데이터라 볼 수 없음.
+
+
+# 8. word cloud 작성 후 분석
+wordcloud2( word_table2 )
+
+# 8.1 배경 및 색상 변경
+wordcloud2( word_table2,
+            color = 'random-light',
+            backgroundColor = 'black')
+
+# 8.2 모양 변경
+wordcloud2( word_table2,
+            fontFamily = '맑은고딕',
+            size = 1.2, color = 'random-light',
+            backgroundColor = 'black',
+            shape = 'star')
+
+# 8.3 선택 색상 반복
+wordcloud2( word_table2, size = 1.6,
+            color = rep_len( c('red','blue'),
+                             nrow( word_table2)))
+
+wordcloud2( demoFreq, size = 1.6,
+            color = rep_len( c('yellowgreen','skyblue'),
+                             nrow( word_table2)))
+
+# 8.4 일정 방향 정렬
+wordcloud2( word_table2,
+            minRotation = -pi / 6,
+            maxRotation = -pi / 6,
+            rotateRatio = 1 )
+
+wordcloud2( demoFreq,
+            minRotation = -pi / 6,
+            maxRotation = -pi / 6,
+            rotateRatio = 1 )
+# 시각화는 결과일 뿐, 시각화에 치중하기 보다는 데이터 분석을 잘하자... 전처리를 잘하자..
+# 전처리를 못하면 시각화도 별로라는 것..
+
+
+
+
+
+
+
+
+
+
---
+++ WorkR/problem/Leeyunji_20191212.R
@@ -0,0 +1,223 @@
+# 
+# 이름 : 이윤지
+# 작성일 : 2019. 12. 11 
+# 제출일 : 2019. 12. 12
+
+# 워드 클라우드( Word Cloud ) 순서
+#
+# 한글 워드클라우드 설치
+# 1. java 실행환경 구축
+# 2. 자료 수집( text 자료 )
+#   2.1 text file 형태로 수집
+#   2.2 web scrapping을 이용하여 수집
+# 3. 명사 추출
+# 4. 추출된 단어( 주로 명사 )에 대한 빈도수 계산 및 시각화
+# 5. word cloud 작성
+# 6. 전처리 과정 수행
+#   6.1 생략된 단어를 사전에 등재
+#   6.2 불필요한 단어 삭제
+# 7. 다시 wordcloud 실행
+
+
+# Sys.setenv( JAVA_HOME = "C:/Program Files/Java/jre1.8.0_231")
+# #필요시 설치
+# install.packages("wordcloud")       # word cloud
+# install.packages("wordcloud2")      # word cloud
+# install.packages("KoNLP")           # 한국어 처리 <- 한국어 처리시 반드시 필요
+# install.packages("RColorBrewer")    # 색상 선택
+# library( wordcloud )
+# library( wordcloud2 )
+# library( KoNLP )
+# library( RColorBrewer )
+# 
+# library( dplyr )
+# library( ggplot2 )
+
+
+#
+# 문1)
+# 20대 국회 개원 여·야 3당 대표 국회연설문에 대해 각각 워드클라우드를 작성하시오.
+# 예제소스 파일은 ‘ex_10-1.txt’, ‘ex_10-2.txt’, ‘ex_10-3.txt’이다.
+
+buildDictionary( ext_dic = "woorimalsam")
+
+# ex_10-1.txt
+chu <- readLines("ex_10-1.txt", encoding = 'UTF-8')
+chu.noun <- sapply( chu, extractNoun, USE.NAMES = F)
+class(chu.noun)
+
+# 리스트를 vector 형으로 변환
+chu.n <- unlist( chu.noun ); chu.n
+cwordcount <- table( chu.n ); cwordcount
+sort.chu <- sort( cwordcount, decreasing = T )
+sort.chu <- sort.chu[-1]
+head(sort.chu)
+
+# 불필요한 단어 삭제
+chu.n <- chu.n[ nchar(chu.n) > 1 ]
+chu.n <- gsub('◇','',chu.n)
+chu.n <- gsub('년','',chu.n)
+chu.n <- gsub('되','',chu.n)
+chu.n <- gsub('드','',chu.n)
+chu.n <- gsub('명','',chu.n)
+chu.n <- gsub('만','',chu.n)
+chu.n <- gsub('아','',chu.n)
+chu.n <- gsub('오','',chu.n)
+chu.n <- gsub('조','',chu.n)
+chu.n <- gsub('중','',chu.n)
+chu.n <- gsub('지','',chu.n)
+chu.n <- gsub('패착입니다','패착',chu.n)
+chu.n <- gsub('때문','',chu.n)
+
+cwordcount <- table(chu.n)
+cwordcount
+
+
+pal2 <- brewer.pal( 6, 'Dark2' )
+wordcloud( names(cwordcount),
+           freq = cwordcount,
+           scale = c( 8, 0.5 ),
+           min.freq = 3,
+           random.order = F,
+           rot.per = 0.1,
+           colors = pal2)
+
+wordcloud2( cwordcount, size = 2 )
+
+
+# ex_10-2.txt
+lee <- readLines("ex_10-2.txt", encoding = 'UTF-8')
+lee.noun <- sapply( lee, extractNoun, USE.NAMES = F)
+head(lee.noun)
+lee.n <- unlist( lee.noun ); lee.n
+lwordcount <- table(lee.n)
+lwordcount
+sort.lee <- sort(lwordcount, decreasing = T )
+sort.lee <- sort.lee[-1]
+
+# 불필요한 단어 삭제
+lee.n <- lee.n[ nchar(lee.n) > 1 ]
+lee.n <- gsub('것','',lee.n)
+lee.n <- gsub('되','',lee.n)
+lee.n <- gsub('저','',lee.n)
+lee.n <- gsub('수','',lee.n)
+lee.n <- gsub('하','',lee.n)
+lee.n <- gsub('등','',lee.n)
+lee.n <- gsub('년','',lee.n)
+lee.n <- gsub('번','',lee.n)
+lee.n <- gsub('중','',lee.n)
+lee.n <- gsub('거','',lee.n)
+lee.n <- gsub('고','',lee.n)
+lee.n <- gsub('그','',lee.n)
+lee.n <- gsub('금','',lee.n)
+lee.n <- gsub('긍','',lee.n)
+lee.n <- gsub('라','',lee.n)
+lee.n <- gsub('내','',lee.n)
+lee.n <- gsub('보','',lee.n)
+lee.n <- gsub('시','',lee.n)
+lee.n <- gsub('중','',lee.n)
+lee.n <- gsub('해왔던국회개혁과','국회개혁',lee.n)
+
+lwordcount <- table(lee.n)
+lwordcount
+
+pal3 <- brewer.pal( 9, 'Blues')[5:9]
+wordcloud( names(lwordcount), freq = lwordcount,
+           scale = c(8,1), # 최대, 최소
+           min.freq = 4,
+           random.order = F, rot.per = .1, colors = pal3)
+
+wordcloud2( lwordcount, size = 2 )
+
+# ex_10-3.txt
+park <- readLines("ex_10-3.txt", encoding = 'UTF-8')
+park.noun <- sapply( park, extractNoun, USE.NAMES = F)
+head(park.noun)
+park.n <- unlist( park.noun ); park.n
+pwordcount <- table( park.n )
+sort.park <- sort( pwordcount, decreasing = T )
+sort.park <- sort.park[-1]
+sort.park
+
+# 불필요한 단어 삭제
+park.n <- park.n[ nchar(park.n) > 1 ]
+park.n <- gsub('하','',park.n)
+park.n <- gsub('것','',park.n)
+park.n <- gsub('수','',park.n)
+park.n <- gsub('한','',park.n)
+park.n <- gsub('드','',park.n)
+park.n <- gsub('저','',park.n)
+park.n <- gsub('년','',park.n)
+park.n <- gsub('대','',park.n)
+park.n <- gsub('원','',park.n)
+park.n <- gsub('하','',park.n)
+park.n <- gsub('◇','',park.n)
+park.n <- gsub('만','',park.n)
+park.n <- gsub('때문','',park.n)
+park.n <- gsub('등','',park.n)
+park.n <- gsub('주도하','주도',park.n)
+park.n <- gsub("'국민",'국민',park.n)
+park.n <- gsub("'대통령",'대통령',park.n)
+park.n <- gsub('40kg에','40kg',park.n)
+park.n <- gsub('국민여러분께서는','국민여러분',park.n)
+park.n <- gsub('더불어민주당에게도','더불어민주당',park.n)
+park.n <- gsub('통령의','통령',park.n)
+
+pwordcount <- table(park.n)
+pwordcount 
+
+
+wordcloud( names(pwordcount), freq = pwordcount,
+           scale = c( 8, 1), min.freq = 5,
+           random.order = F, rot.per = 0.1, colors = pal2)
+
+wordcloud2( pwordcount, size = 3, shape = 'star' )
+
+
+# 문2)
+# 스티브 잡스의 스탠포드 대학 졸업식 연설문에 대해 워드클라우드를 작성하시오.
+
+steve <- readLines("ex_10-4.txt", encoding = 'UTF-8')
+steve
+
+steve.noun <- sapply( steve, extractNoun, USE.NAMES = F )
+head(steve.noun)
+sn <- unlist( steve.noun ); sn
+swordcount <- table(sn)
+sort.sn <- sort( swordcount, decreasing = T )
+sort.sn <- sort.sn[-3]
+sort.sn
+
+# swordcount <- table(sort.sn)
+# swordcount
+
+wordcloud( names( sort.sn ), freq = sort.sn,
+           scale = c( 8, 1), min.freq = 3,
+           random.order = F, rot.per = 0.1, colors = pal2)
+
+wordcloud2( swordcount, size = 3 )
+
+
+# 문3) 
+# 오바마 대통령의 데통령 당선 연설문에 대해 워드클라우드를 작성하시오
+# Tip. 예제소스 파일은 ‘ex_10-5.txt’이다.
+
+obama <- readLines("ex_10-5.txt", encoding = 'UTF-8' )
+obama.noun <- sapply( obama, extractNoun, USE.NAMES = F )
+head(obama.noun)
+on <- unlist(obama.noun); on
+owordcount <- table(on)
+sort.obama <- sort( owordcount, decreasing = T )
+sort.obama <- sort.obama[-1]
+head(sort.obama)
+
+owordcount
+
+wordcloud( names(owordcount), freq = owordcount,
+           min.freq = 3, scale = c( 9, 0.7 ),
+           random.order = F, rot.per = 0.2, colors = pal2)
+wordcloud2( owordcount, color = 'random-light',
+            backgroundColor = 'black',
+            size = 2 )
+
+
